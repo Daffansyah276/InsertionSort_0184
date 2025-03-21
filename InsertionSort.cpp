84f088b749c1e09255c39e1b81c9689af3b95e4d
Daffansyah276 <daffansyaharya@gmail.com>
2025-03-21 09:53:44 +0700

    fungsi

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -38,3 +38,19 @@ void InsertionSort()
 int temp;
 int j, i; 
 
+for (i = 1; i <= n; i++)
+{//step 1
+
+temp = arr[i]; //step 2
+
+j = i - 1; //step 3 
+
+while (j >= 0 && arr[j] > temp) //step 4
+{
+arr [j + 1 ] = arr [j]; // step 4a
+j--; //step 4b  
+}
+
+arr[j + 1 ] = temp; //step 5 
+}
+}
