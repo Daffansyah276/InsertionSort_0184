966de46c64d7b79d7f9405dc919ec7178a1a4bce
Daffansyah276 <daffansyaharya@gmail.com>
2025-03-21 09:44:07 +0700

    memasukkan fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -10,4 +10,25 @@ while (true)
 {
 cout << "Masukkan Jumlah data pada Array : ";
 cin << n; 
+
+if ( n => 20)
+{ // membuat kondisi n tidak lebih dari 20 
+break;    
 }
+else
+{
+cout << "\n Array yang ada masukkan maksimal 20 Elemen.\n; "
+}
+}
+cout << endl;
+cout << "=======================" << endl;
+cout << "Masukkan Element Array" << endl;
+cout << "=======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+cout << "Data ke-" << (i + 1) << :;
+cin >> arr[i];
+}
+}
+
